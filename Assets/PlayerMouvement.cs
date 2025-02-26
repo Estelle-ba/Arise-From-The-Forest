e11179ec1b01ac05f55c07139f4609c2eefe59a0
ghazi-code <ghazi.khiari@edu.esiee-it.fr>
2025-02-26 10:38:34 +0100

    serialzed add

--- Assets/PlayerMouvement.cs
+++ Assets/PlayerMouvement.cs
@@ -6,7 +6,9 @@ using UnityEngine.Rendering;
 public class PlayerMouvement : MonoBehaviour
 {
     private float horizontal;
+    [SerializeField]
     private float speed = 8f;
+    [SerializeField]
     private float JumpingPower = 16f;
     private bool isFacingRight = true;
     private bool isJumping = true;
